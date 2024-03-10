16c9ef6eaf3ba7b0b00e5f52c2d5fc7b2b5822c9
Raghurambeldon <157978303+Raghurambeldon@users.noreply.github.com>
2024-03-10 15:23:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+function firstone() {
+    var x = document.getElementById("first-one");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function secondone() {
+    var x = document.getElementById("second-one");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function third() {
+    var x = document.getElementById("third");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function forth() {
+    var x = document.getElementById("forth");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function fifth() {
+    var x = document.getElementById("fifth");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function sixth() {
+    var x = document.getElementById("sixth");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
